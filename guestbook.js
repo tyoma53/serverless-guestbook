685c6b439c7d07bc6fd0f05d02f5dd05f2d74246
tyoma53 <80357845+tyoma53@users.noreply.github.com>
2022-06-08 16:25:17 +0300

    Update guestbook.js
    apiUrl update

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d4493119.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
